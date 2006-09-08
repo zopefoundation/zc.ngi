b32b59485cc8bf9ec7621469654c26da03fead7e
Jim Fulton <jim@zope.com>
2006-09-08 19:01:51 +0000

    Added a sized-message adapter.

--- src/zc/ngi/adapters.py
+++ src/zc/ngi/adapters.py
@@ -15,6 +15,7 @@
 
 $Id$
 """
+import struct
 
 class Lines:
 
@@ -39,4 +40,56 @@ class Lines:
         self.handler.handle_close(self, reason)
 
 
+class Sized:
+
+    def __init__(self, connection):
+        self.connection = connection
+        self.close = connection.close
+
+    def setHandler(self, handler):
+        self.handler = handler
+        self.input = []
+        self.want = 4
+        self.got = 0
+        self.getting_size = True
+        self.connection.setHandler(self)
+
+    def handle_input(self, connection, data):
+        self.got += len(data)
+        self.input.append(data)
+        while self.got >= self.want:
+            extra = self.got - self.want
+            if extra == 0:
+                collected = ''.join(self.input)
+                self.input = []
+            else:
+                input = self.input
+                self.input = [data[-extra:]]
+                input[-1] = input[-1][:-extra]
+                collected = ''.join(input)
+
+            self.got = extra
+
+            if self.getting_size:
+                # we were recieving the message size
+                assert self.want == 4
+                if collected == '\xff\xff\xff\xff':
+                    # NULL message. Ignore
+                    continue
+                self.want = struct.unpack(">I", collected)[0]
+                self.getting_size = False
+            else:
+                self.want = 4
+                self.getting_size = True
+                self.handler.handle_input(self, collected)
+
+    def handle_close(self, connection, reason):
+        self.handler.handle_close(self, reason)
+
+    def write(self, message):
+        if message is None:
+            self.connection.write('\xff\xff\xff\xff')
+        else:
+            self.connection.write(struct.pack(">I", len(message)))
+            self.connection.write(message)
     
