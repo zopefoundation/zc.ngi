1e1cc0bd852e75eeca8b8115a8e51f4e558ec5af
Jim Fulton <jim@zope.com>
2009-06-29 17:17:50 +0000

    Updated version.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@
 #
 ##############################################################################
 
-name, version = 'zc.ngi', '0'
+name, version = 'zc.ngi', '1.1.1'
 
 import os
 from setuptools import setup, find_packages
