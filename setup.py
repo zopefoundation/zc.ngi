494975440da7d13ce1c9b834a818004a2c255d8b
Jim Fulton <jim@zope.com>
2011-12-10 17:20:58 +0000

    *** empty log message ***

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@
 #
 ##############################################################################
 
-name, version = 'zc.ngi', '0'
+name, version = 'zc.ngi', '2.0.0'
 
 from setuptools import setup, find_packages
 
